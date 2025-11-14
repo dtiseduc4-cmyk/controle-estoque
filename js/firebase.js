a0f5072439801cbf9e82db2e3e3add03ae650739
dtiseduc4-cmyk <dtiseduc4@gmail.com>
2025-11-14 16:55:35 -0300

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,9 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+
+const firebaseConfig = {
+  // ...suas configs
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
